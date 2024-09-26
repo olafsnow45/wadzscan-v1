f0771f0aceaf49268202460d5ec84caff8d43c03
olafsnow45 <olafsnow0405@gmail.com>
2024-09-26 16:10:02 +0900

    removed dark switch button & updated config testnet

--- config/runtime.exs
+++ config/runtime.exs
@@ -73,7 +73,7 @@ config :block_scout_web, BlockScoutWeb.Chain,
   has_emission_funds: false,
   show_maintenance_alert: ConfigHelper.parse_bool_env_var("SHOW_MAINTENANCE_ALERT"),
   enable_testnet_label: ConfigHelper.parse_bool_env_var("SHOW_TESTNET_LABEL"),
-  testnet_label_text: System.get_env("TESTNET_LABEL_TEXT", " Wadzchain")
+  testnet_label_text: System.get_env("TESTNET_LABEL_TEXT", "Testnet")
 
 config :block_scout_web, :footer,
   logo: System.get_env("FOOTER_LOGO"),
